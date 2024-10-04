ed8c6c52be57a5aeec6817326dc4c15ba57c127b
unknown <bchwangdv@gmail.com>
2024-10-04 20:07:25 +0900

    241004

--- theZD/src/main/java/com/bvrg/theZD/search/controller/SearchController.java
+++ theZD/src/main/java/com/bvrg/theZD/search/controller/SearchController.java
@@ -9,19 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SearchController {
 
 	@GetMapping("searchResult")
-	public String getSearchList(@RequestParam("searchCondition") String searchCondition,
+	public String getSearchList(
 								@RequestParam("nickname") String nickname,
 								Model model) {
-		int conditionNo = 0;
-		if(nickname == "") {
-			//경고창, redirect
-		} else if (searchCondition.equals("officialMatch")) {
-			conditionNo = 50;
-		} else if (searchCondition.equals("classic1on1")) {
-			conditionNo = 40;
-		} else if (searchCondition.equals("managerMode")) {
-			conditionNo = 52;
-		}
+		int conditionNo = 50; // default 공식경기 검색
 		
 		model.addAttribute("conditionNo", conditionNo);
 		model.addAttribute("nickname", nickname);
