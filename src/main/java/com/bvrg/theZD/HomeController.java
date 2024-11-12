d2ad64557d38b4483235f6811759c3500eeecf67
bchwangdv <bchwang.dv@gmail.com>
2024-11-12 19:59:13 +0900

    241112

--- src/main/java/com/bvrg/theZD/HomeController.java
+++ src/main/java/com/bvrg/theZD/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping("main")
+	@GetMapping(value={"/","main"})
 	public String main() {
 		return "main";
 	}
